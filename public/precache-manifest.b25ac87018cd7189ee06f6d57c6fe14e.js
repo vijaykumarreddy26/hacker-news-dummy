30545fac7d5651aeb4f5a98ce47f138aac23382b
vijay kumar reddy <vijaykumarreddy26@gmail.com>
2020-06-09 08:13:27 +0530

    adding public files

---
+++ public/precache-manifest.b25ac87018cd7189ee06f6d57c6fe14e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebe06c93dce1134852f4a8978cea4b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ebcc3621d2da6cc1917",
+    "url": "/static/css/main.b6f0f697.chunk.css"
+  },
+  {
+    "revision": "d5b7db33317c5be94d62",
+    "url": "/static/js/2.5cf4dbb2.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.5cf4dbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebcc3621d2da6cc1917",
+    "url": "/static/js/main.24f2cab2.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
